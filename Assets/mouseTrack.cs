8aae0ef3752ce5d01ec6ed0496d83450b02b8ebc
Alexandre Knijn <aknijn1@lsu.edu>
2017-02-01 14:50:37 -0600

    Basic game (running and jumping around and incomplete boomerang)

---
+++ Assets/mouseTrack.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseTrack : MonoBehaviour {
+
+    GameObject playerObj;
+    Transform playerTrans;
+    Transform boomerangTrans;
+    Vector3 playerPos;
+    Vector3 mousePos;
+    Vector3 boomerangPos;
+    float difX;
+    float difY;
+    float angle;
+    float currentTime;
+    bool thrown;
+
+
+    void Start ()
+    {
+        playerObj   = GameObject.Find("player");
+        playerTrans = playerObj.GetComponent<Transform>();
+        boomerangTrans = gameObject.GetComponent<Transform>();
+    }
+	void Update ()
+    {
+        playerPos = playerTrans.position;
+        mousePos = Input.mousePosition;
+        difX = playerPos.x - mousePos.x;
+        difY = playerPos.y - mousePos.y;
+
+        angle = Mathf.Atan(difY/difX);
+        if(difX < 0.0f && difY < 0.0f && !thrown)
+        {
+            boomerangPos = new Vector3(playerPos.x + 1.5f * Mathf.Cos(angle), playerPos.y + 1.5f * Mathf.Sin(angle), 0.0f);
+        }
+        else if(difX > 0.0f && !thrown)
+        {
+            angle = -angle;
+            boomerangPos = new Vector3(playerPos.x - 1.5f * Mathf.Cos(angle), playerPos.y + 1.5f * Mathf.Sin(angle), 0.0f);
+        }
+        else if(difY < 0.0f && !thrown)
+        {
+            boomerangPos = new Vector3(playerPos.x + 1.5f * Mathf.Cos(angle), playerPos.y + 1.5f * Mathf.Sin(angle), 0.0f);
+        }
+        else if(!thrown)
+        {
+            boomerangPos = new Vector3(playerPos.x + 1.5f * Mathf.Cos(angle), playerPos.y + 1.5f * Mathf.Sin(angle), 0.0f);
+        }
+        gameObject.GetComponent<Transform>().position = boomerangPos;
+        if(Input.GetMouseButtonDown(0) && !thrown)
+        {
+            thrown = true;
+            currentTime = Time.time;
+        }
+        if(thrown)
+        {
+            gameObject.GetComponent<Transform>().Translate(Mathf.Sin((Time.time - currentTime)*3)*Mathf.Cos(angle), Mathf.Sin((Time.time - currentTime)*3) * Mathf.Sin(angle),0);
+        }
+        if((Time.time - currentTime) == (2*Mathf.PI)  && thrown)
+        {
+            Destroy(GameObject.Find("Boomerang"));
+        }
+
+    }
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    Rigidbody2D playerRB;
+    BoxCollider2D playerBC;
+    GameObject groundObj;
+    float move      = 0.0f;
+    float maxSpd    = 6.0f;
+    float jmpStr    = 300.0f;
+    float fallStr   = -50.0f;
+    float fallSpd   = 1.0f;
+    bool onTheGround= true;
+    int jump = 0;
+
+    private void Start ()
+    {
+        playerRB = gameObject.GetComponent<Rigidbody2D>();
+        playerBC = gameObject.GetComponent<BoxCollider2D>();
+        groundObj= GameObject.Find("floor");
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (move < maxSpd)
+            {
+                move = move + 2.0f;
+            }
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            if (move > (maxSpd * -1))
+            {
+                move = move - 2.0f;
+            }
+        }
+        else
+        {
+            if (move > 0.0f)
+            {
+                move = move - .5f;
+            }
+            else if (move < 0.0f)
+            {
+                move = move + .5f;
+            }
+        }
+        playerRB.velocity = new Vector2(move, playerRB.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.W) && jump<1)
+        {
+            jump++;
+            playerRB.AddForce(new Vector2(0f, jmpStr));
+            onTheGround = false;
+        }
+
+        if (playerBC.IsTouching(groundObj.GetComponent<BoxCollider2D>()))
+        {
+            jump = 0;
+        }
+                    
+        if(Input.GetKey(KeyCode.S))
+        {
+            playerRB.AddForce(new Vector2(0f, fallStr));
+        }
+
+        
+
+    }
+}
