9bcbaa38499643fd5ee8ad45e86cc665c6828547
Miles Robicheaux <mrobi37@tigers.lsu.edu>
2017-02-13 16:46:56 -0600

    So something isn't working right with the camera script. It worked when I had a seperate unity project, but putting everything into the main scene here has caused something to go wrong.
    Here are a couple sources on dynamic cameras:
    http://clutchware.co.uk/developing-a-2d-dynamic-camera-in-unity/
    http://www.gamasutra.com/blogs/NicholasDiMucci/20150217/236492/Single_Camera_System_for_Four_Players.php

---
+++ Assets/LaunchTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchTest : MonoBehaviour {
+	public Rigidbody2D rb;
+	public float xForce;
+	public float yForce;
+
+	public float d; 			//base damage of move
+	public float b; 			//base knockback of move
+	public float s; 			//knockback scaling/growth
+	public Vector2 ld; 			//launch direction relative to player
+
+	public float w; 			//weight of character
+	public float f; 			//max fall speed of character
+	public bool stunned; 		//whether or not a character 
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			Debug.Log ("force applied");
+			rb.AddForce (new Vector2 (xForce, yForce));
+			//rb.AddForce(transform.up *10);
+		}
+	}
+}
