be2ab40b93023478c5e028408fcd45adedfdf4b2
Alexandre Knijn <aknijn1@lsu.edu>
2017-02-18 13:29:50 -0600

    mostly unnoteable changes changed some variable names

--- Assets/Player/mouseTrack.cs
+++ Assets/Player/mouseTrack.cs
@@ -8,10 +8,10 @@ public class mouseTrack : MonoBehaviour {
 
     GameObject playerObj;
     Transform playerTrans;
-    Transform boomerangTrans;
+    Transform gunTrans;
     Vector3 playerPos;
     Vector3 mousePos;
-    Vector3 boomerangPos;
+    Vector3 gunPos;
 
     float difX;
     float difY;
@@ -25,7 +25,7 @@ public class mouseTrack : MonoBehaviour {
     {
         playerObj   = GameObject.Find("player");
         playerTrans = playerObj.GetComponent<Transform>();
-        boomerangTrans = gameObject.GetComponent<Transform>();
+        gunTrans = gameObject.GetComponent<Transform>();
     }
 	void Update ()
     {
@@ -35,37 +35,39 @@ public class mouseTrack : MonoBehaviour {
         difY = playerPos.y - mousePos.y;
 
         angle = Mathf.Atan(difY/difX);
-        if (difX < 0.0f && difY < 0.0f)
+        if (difX < 0.0f && difY < 0.0f)//(1)
         {
             gunX = playerPos.x + 1.5f * Mathf.Cos(angle);
             gunY = playerPos.y + 1.5f * Mathf.Sin(angle);
-            boomerangPos = new Vector3(gunX, gunY, 0.0f);
+            gunPos = new Vector3(gunX, gunY, -4.0f);
         }
-        else if (difX > 0.0f)
+        else if (difX < 0.0f && difY > 0.0f)//(2)
         {
-            angle = -angle;
-            gunX = playerPos.x - 1.5f * Mathf.Cos(angle);
-            gunY = playerPos.y + 1.5f * Mathf.Sin(angle);
-            boomerangPos = new Vector3(gunX, gunY, 0.0f);
-        }
-        else if (difY < 0.0f)
-        {
+            //angle -= 90;
             gunX = playerPos.x + 1.5f * Mathf.Cos(angle);
             gunY = playerPos.y + 1.5f * Mathf.Sin(angle);
-            boomerangPos = new Vector3(gunX,gunY, 0.0f);
+            gunPos = new Vector3(gunX, gunY, -4.0f);
         }
-        else
+        else if (difY > 0.0f && difY < 0.0f)//(3)
         {
+            //angle += 90;
             gunX = playerPos.x + 1.5f * Mathf.Cos(angle);
             gunY = playerPos.y + 1.5f * Mathf.Sin(angle);
-            boomerangPos = new Vector3(gunX, gunY, 0.0f);
+            gunPos = new Vector3(gunX,gunY, -4.0f);
         }
-        gameObject.GetComponent<Transform>().position = boomerangPos;
+        else//(4)
+        {
+            //angle += 180;
+            gunX = playerPos.x + 1.5f * Mathf.Cos(angle);
+            gunY = playerPos.y + 1.5f * Mathf.Sin(angle);
+            gunPos = new Vector3(gunX, gunY, -4.0f);
+        }
+        gameObject.GetComponent<Transform>().position = gunPos;
 
         if (Input.GetMouseButtonDown(0))
         {
             Rigidbody2D bullet;
-            bullet = (Rigidbody2D)Instantiate(Bullet, boomerangPos, Quaternion.identity);
+            bullet = (Rigidbody2D)Instantiate(Bullet, gunPos, Quaternion.identity);
             bullet.AddForce(new Vector2(bulletSpeed*(gunX-playerPos.x), bulletSpeed * (gunY -playerPos.y)));
         }
 
