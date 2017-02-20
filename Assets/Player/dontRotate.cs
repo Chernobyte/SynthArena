b0f53d8888b13f44d2bd8b09a63931c1da92e9dc
Alexandre Knijn <aknijn1@lsu.edu>
2017-02-20 14:48:03 -0600

    Dummy player doesn't rotate.

---
+++ Assets/Player/dontRotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dontRotate : MonoBehaviour {
+
+	
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+        gameObject.GetComponent<Transform>().rotation = Quaternion.identity;
+    }
+}
