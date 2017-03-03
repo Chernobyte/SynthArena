7fa412acdde2bdeee57f5106f18b52fa9219e9df
Miles Robicheaux <mrobi37@tigers.lsu.edu>
2017-03-03 12:01:10 -0600

    Added a new script "gunMouseTrack" to replace the "mouseTrack" script. The only thing left to do is to get the "gun" to rotate in the direction of the mouse

---
+++ Assets/Player/gunMouseTrack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunMouseTrack : MonoBehaviour {
+
+	public Transform target;
+	public float fRadius = 3.0f;
+	private Transform pivot;
+	// Use this for initialization
+	void Start () {
+		pivot = new GameObject ().transform;
+		transform.parent = pivot;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 v3Pos = Camera.main.WorldToScreenPoint (target.position);
+		v3Pos = Input.mousePosition - v3Pos;
+		float angle = Mathf.Atan2 (v3Pos.y, v3Pos.x) * Mathf.Rad2Deg;
+
+		//pivot.position = target.position;
+		//pivot.rotation = Quaternion.AngleAxis (angle, Vector3.forward);
+		v3Pos = Quaternion.AngleAxis(angle, Vector3.forward) * (Vector3.right * fRadius);
+		transform.position = target.position + v3Pos;
+	}
+}
