4f1229353fd320aa978ee8eec88844fe722072da
Matthew Williams <Matthew Williams>
2017-03-15 00:43:20 -0500

    Added SFX for gameplay

--- ToneAssignment2/sketch.js
+++
@@ -1,9 +0,0 @@
-var osc = [12];
-var env = new Tone.AmplitudeEnvelope();
-function setup(){
-for(var i = 0; i < 12; i++){
-  osc[i] = new Tone.FMOscillator(440, "triangle", "square");
-  }
-}
-
-osc[0].toMaster().start();
